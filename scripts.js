ff576ebb70f2e77b569dff6b085c8022c24db072
Peter Ramsing <peter.ramsing@naturalpoint.com>
2016-08-19 12:31:36 -0700

    Initial add of space mode
    It’s a bit buggy but it’s cool.

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.getElementsByClassName('js-floor-button')[0].onclick = function() {
+  if (this.dataset.floor === 'space') {
+    document.body.className = 'space-mode';
+
+    setTimeout(function () {
+      document.getElementsByClassName('js-elevator--floor-indicator')[0].innerHTML = 'SPACE!';
+    }, 2500);
+  }
+};
